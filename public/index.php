823a974be290ab27af866f72a092ed1abad494c6
Thaïs Brun <travailthais@outlook.fr>
2022-12-23 22:56:51 +0100

    Installation de Symfony et avancement du projet

--- public/index.php
+++ public/index.php
@@ -1,9 +1,322 @@
-<?php
 
-use App\Kernel;
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Vente, location et demi-pension sur chevaux et poneys</title>
+<!-- CSS et JS -->
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <script src="script.js"></script>
+</head>
+<body>
+<!-- entete Shopony -->
+<nav class="navbar" role="navigation" aria-label="main navigation">
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+        <div class="navbar-brand">
+    <a class="navbar-item" href="index.php">
+      <img src="img/logo.png" id="logo">
+    </a>
+      <a class="navbar-item">
+       <h1>Shopony</h1>
+      </a>
+      </div>
+    </div>
+</nav>
+<!-- bordereau d'accueil vente / achat -->
+<div class="columns">
+  <div class="column is-half">
+    <img src="img/background3.jpg" id="frontW">
+    <div class="center"><h2>Je vends mon poney</h2>
+    </div>
+    <div class="centerB">
+    <button class="button is-medium" id="buttonSale">Vendre</button></div>
+</div>
+  <div class="column is-half">
+  <img src="img/background2.jpg" id="frontW">
+  <div class="centerA"><h2>J'achète un poney</h2>
+  </div>
+  <div class="centerBA">
+  <button class="button is-medium" id="buttonBuy">Acheter</button>
+</div>
+  </div>
+</div>
+<!-- Annonces 4 par 4 : 12 de vente -->
+<h2>Equidés à vendre </h2>
+<br>
+<br>
+<div class="columns">
+  <div class="column"><div class="card">
+  <div class="card-image">
+  <img src="img/Quid.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Poney B</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 2000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+    <img src="img/Bellium.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Poney C</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 5000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+  <img src="img/Balzac.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Cheval</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;5000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+    <img src="img/Chuky.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Shetland</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 600$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  </div>
+</div>
+<h3>Voir plus d'équidés à vendre</h3>
+<br>
+<br>
+<!-- Annonces 4 par 4 : 12 de location -->
+<h2>Equidés à louer</h2>
+<br>
+<br>
+<div class="columns">
+  <div class="column"><div class="card">
+  <div class="card-image">
+  <img src="img/Quid.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Poney B</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 2000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+    <img src="img/Bellium.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Poney C</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 5000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+  <img src="img/Balzac.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Cheval</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;5000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+    <img src="img/Chuky.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Shetland</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 600$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  </div>
+</div>
+<h3>Voir plus</h3>
+<br>
+<br>
+<!-- Annonces 4 par 4 : 12 de demi-pension -->
+<h2>Poneys à prendre en DP</h2>
+<br>
+<br>
+<div class="columns">
+  <div class="column"><div class="card">
+  <div class="card-image">
+  <img src="img/Quid.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Poney B</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 2000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+    <img src="img/Bellium.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Poney C</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 5000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+  <img src="img/Balzac.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Cheval</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;5000$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="column">
+    <div class="card">
+  <div class="card-image">
+    <img src="img/Chuky.jpg" id="frontS">
+  </div>
+  <div class="card-content">
+    <div class="media">
+        <p class="title is-4">Shetland</p><p class="title is-6">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 600$</p>
+    </div>
+    <br>
+    <div class="content">
+    <button class="button is-link is-outlined is-fullwidth">En savoir plus</button>
+      <br>
+      <time datetime="2016-1-1">Posté à 22H04 - 3 Septembre 2022</time>
+    </div>
+  </div>
+</div>
+  </div>
+  </div>
+</div>
+<h3>Voir plus</h3>
+<br>
+<br>
+<br>
+</body>
+<!-- pied de page -->
+<footer>
+<div class="columns">
+  <div class="column">
+    Mentions légales
+  </div>
+  <div class="column">
+    Comment ça marche ?
+  </div>
+  <div class="column">
+    A propos de Shopony
+  </div>
+  <div class="column">
+    M'inscrire
+</div>
+  <div class="column">
+Me connecter
+</div>
+  <div class="column">
+Contact
+  </div>
+</div>
+</footer>
+</html>
